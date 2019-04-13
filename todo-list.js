6ab1c33c89b9374c4e65d4176ebd372fb558bcf2
Patrick Goulding <patrickgoulding@gmail.com>
2019-04-12 22:10:33 -0600

    add clone and remove functions

--- todo-list.js
+++ todo-list.js
@@ -5,11 +5,20 @@ class ToDoList{
     this.tasks = [];
     this.urgent = urgent || false;
   }
-  saveToStorage(){
-
+  saveToStorage(lists) {
+    localStorage.setItem('idea-card', JSON.stringify(lists));
   }
-  deleteFromStorage(){
-
+  deleteFromStorage(index) {
+    lists.splice(index, 1);
+    this.saveToStorage(lists);
+  }
+  updateTitle(idea, editedTitle) {
+    idea.title = editedTitle;
+    this.saveToStorage(lists);
+  }
+  urgent() {
+    this.urgent = !this.urgent;
+    this.saveToStorage(lists);
   }
   updateToDo(){
 
