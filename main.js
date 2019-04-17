457216466cf95d3e63a147ea67f56421c50f752f
Patrick Goulding <patrickgoulding@gmail.com>
2019-04-17 03:51:04 -0600

    final, probably

--- main.js
+++ main.js
@@ -2,9 +2,7 @@
 var inputTitle = document.querySelector('#input-title');
 var inputTaskItem = document.querySelector('#input-item');
 var inputSearch = document.querySelector('#input-search');
-
 var outputArray = document.getElementsByClassName('sidebar__task-list');
-
 var btnTaskListItemDelete = document.getElementsByClassName('img__task-delete');
 var btnAddTask = document.querySelector('#add-task-icon');
 var btnCreateTaskList = document.querySelector('#btn-make-task');
@@ -22,7 +20,6 @@ btnCreateTaskList.addEventListener('click', addTaskToArray);
 btnClear.addEventListener('click', clearTaskListBtn);
 btnFilterUrgent.addEventListener('click', toggleShowUrgent);
 inputSearch.addEventListener('input', searchLists);
-
 /*---------- Functions -----------------*/
 function startCheckYoSelf() {
   fetchLists()
@@ -64,9 +61,7 @@ function reinstateLists(i) {
 function searchLists(e) {
   e.preventDefault()
   var searchQuery = inputSearch.value.toLowerCase();
-  var searchResults = lists.filter(card =>
-      card.title.toLowerCase().includes(searchQuery)
-  );
+  var searchResults = lists.filter(card => card.title.toLowerCase().includes(searchQuery));
   cardsArea.innerHTML = '';
   searchResults.forEach(list => addCardToDOM(list));
 }
@@ -76,7 +71,7 @@ function toggleShowUrgent(e) {
   btnFilterUrgent.classList.toggle('urgent');
   if(btnFilterUrgent.classList.contains('urgent')) {
     var filterResults = lists.filter(list => list.urgent === true);
-  }else{
+  } else {
     var filterResults = lists.filter(list => list.urgent === list.urgent)
   }
   cardsArea.innerHTML = "";
@@ -95,10 +90,7 @@ function addTaskToArray(e){
   e.preventDefault()
   var taskArray = []
  for (let i = 0; i < outputArray.length; i++) {
-   var newTask = {
-     id: `1${i}${Date.now()}`,
-     done: false,
-     content: outputArray[i].innerText
+   var newTask = {id: `1${i}${Date.now()}`, done: false, content: outputArray[i].innerText
    }
    taskArray.push(newTask)
  }
@@ -149,7 +141,7 @@ function cardActions(e) {
     urgentify(target);
   }
   if (target.matches('.card__task-checkbox')){
-    checkItemDOM(target);
+    changeCheckbox(target)
   }
   toggleNoLists()
 }
@@ -188,13 +180,24 @@ function urgentBackground(taskList) {
   }
 }
 
-function checkItemDOM(target) {
-  if(target.getAttribute('src', 'images/checkbox.svg') === true){
-    target.setAttribute('src', 'images/checkbox-active.svg')
-    debugger;
-  } else {
-    target.setAttribute('src', 'images/checkbox.svg')
-  }
+// function checkItemDOM(target) {
+//   if(target.getAttribute('src', 'images/checkbox.svg') === true){
+//     target.setAttribute('src', 'images/checkbox-active.svg')
+//     debugger;
+//   } else {
+//     target.setAttribute('src', 'images/checkbox.svg')
+//   }
+// }
+
+function changeCheckbox(target) {
+  if (target.firstChild.src === "images/checkbox.svg"){
+      target.firstChild.src = "images/checkbox-active.svg";
+    } else {
+      target.firstChild.src = "images/checkbox.svg";
+    }
+  // target.firstChild.src = (target.firstChild.src === unchecked) ? checked : unchecked;
+  console.log(target.firstChild.src)
+  console.log(target)
   markItems(target)
 }
 
